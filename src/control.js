653b40a961f1b0baced89c4f40b719f807c674e5
amuseDave <amuseddavid@gmail.com>
2024-07-15 19:49:41 +0300

    add html css

--- src/control.js
+++ src/control.js
@@ -6,6 +6,10 @@ let distance = 0;
 let current = 0;
 let runs = [];
 let map, marker, popup;
+let popups = [];
+let markers = [];
+let numOfRuns = 0;
+let ok;
 
 var myIcon = L.icon({
   iconUrl: "../images/running.svg",
@@ -17,10 +21,23 @@ var myIcon = L.icon({
 function addLines(ev) {
   coords.push([ev.latlng.lat, ev.latlng.lng]);
 
-  addMarkerAndPopUp(coords[0]);
+  marker = addMarker(coords[0]);
+
+  coords.length === 1 ? addPopUp(coords[0], "🚩Start") : console.log("work");
+
   L.polyline(coords, { color: "#002223" }).addTo(map);
   if (coords.length >= 2) {
     calculateDistance();
+    addPopUp(
+      coords[coords.length - 1],
+      `🏁Finish`,
+      true,
+      `<p class="text-tool-tip current-distance">🛣️ ${distance}km</p> <div class="submit"><input class="hidden" type="number" placeholder="Minutes"> <button class="ok"> Ok </button></div>`
+    );
+    ok = document.querySelector(".ok");
+    ok.addEventListener("click", () => {
+      document.querySelector(".submit").style.right = "-90px";
+    });
   }
 }
 
@@ -28,16 +45,29 @@ function createRun() {
   runs.push(new Run());
 }
 
-function addMarkerAndPopUp(crd) {
-  marker = L.marker(crd, {
+function addMarker(crd) {
+  return L.marker(crd, {
     icon: myIcon,
   }).addTo(map);
-
-  popup = L.popup(crd, {
-    content: "<p>🚩Start</p>",
+}
+function addPopUp(crd, content, close = false, content2 = "") {
+  return L.popup(crd, {
+    minWidth: 60,
+    maxWidth: 110,
+    maxHeight: 50,
+    autoClose: false,
+    closeOnClick: close,
+    className: `popup`,
     closeButton: false,
+    closeOnEscapeKey: false,
+    close: false,
+    content: `<p><span class="run run${numOfRuns}">#${
+      numOfRuns + 1
+    }</span>${content}<br />${content2}</p>`,
+    // content: `<p class="text-tool-tip">${content}</p> ${content2}`,
   }).openOn(map);
 }
+
 function removeMarker() {
   marker.remove();
 }
