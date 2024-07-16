21431d483d3e3fd6c3ae1d3b019c9d368782b3e0
amuseDave <amuseddavid@gmail.com>
2024-07-16 10:06:43 +0300

    add overlay functionality for guide

--- src/RunClass.js
+++ src/RunClass.js
@@ -1,10 +1,11 @@
 export default class Run {
-  constructor(date, time, coords, distance, color) {
+  constructor(date, time, coords, distance, color, id) {
     this.date = date;
     this.time = time;
     this.distance = distance;
     this.coords = coords;
     this.color = color;
+    this.id = id;
     this.speed;
     this.location;
   }
--- src/control.js
+++ src/control.js
@@ -4,10 +4,10 @@ import Run from "./RunClass.js";
 let coords = [];
 let distance = 0;
 let currentCord = 0;
-let runs = [];
+let runs = JSON.parse(localStorage.getItem("runDave2024")) || [];
+console.log(runs);
 let map, clicked, okBttn, inputMin, popup, marker;
 let numOfRuns = runs.length;
-let runcoords = [];
 
 var myIcon = L.icon({
   iconUrl: "../images/running.svg",
@@ -15,6 +15,24 @@ var myIcon = L.icon({
   iconAnchor: [30, 40],
   popupAnchor: [0, 0],
 });
+
+function displayRuns() {
+  runs.forEach((run) => {
+    addPopUp(run.coords[0], "🚩Start", undefined, undefined, run.id);
+    addPopUp(
+      run.coords[run.coords.length - 1],
+      `🏁Finish`,
+      false,
+      `<p ${generateStyle(run.id)} class="text-tool-tip current-distance">🛣️ ${
+        run.distance
+      }km</p>`,
+      run.id
+    );
+    addMarker(run.coords[0]);
+    L.polyline(run.coords, { color: generateColor(+run.id) }).addTo(map);
+  });
+}
+// localStorage.clear("runDave2024");
 
 const colorPalette = [
   "#1E90FF", // Dodger Blue
@@ -38,19 +56,19 @@ const colorPalette = [
   "#808080", // Gray
 ];
 
-function generateColor() {
-  return colorPalette[numOfRuns % colorPalette.length];
+function generateColor(id) {
+  return colorPalette[id % colorPalette.length];
 }
-function generateStyle() {
-  return `style="text-shadow: 0px 0px 5px ${generateColor()}"`;
+function generateStyle(id) {
+  return `style="text-shadow: 0px 0px 5px ${generateColor(id)}"`;
 }
 function addLines(ev) {
   coords.push([ev.latlng.lat, ev.latlng.lng]);
   addMarker(coords[0]);
-  coords.length === 1 ? addPopUp(coords[0], "🚩Start") : null;
-  console.log(generateColor());
-  console.log(numOfRuns);
-  L.polyline(coords, { color: generateColor() }).addTo(map);
+  coords.length === 1
+    ? addPopUp(coords[0], "🚩Start", undefined, undefined, numOfRuns)
+    : null;
+  L.polyline(coords, { color: generateColor(numOfRuns) }).addTo(map);
   if (coords.length >= 2) {
     if (okBttn) okBttn.remove();
     if (inputMin) inputMin.remove();
@@ -60,7 +78,8 @@ function addLines(ev) {
       coords[coords.length - 1],
       `🏁Finish`,
       true,
-      generateToolTipText()
+      generateToolTipText(),
+      numOfRuns
     );
     okBttn = document.querySelector(".ok");
     okBttn.addEventListener("click", () => {
@@ -71,7 +90,6 @@ function addLines(ev) {
           return;
         }
         popup.remove();
-        // date, time, coords, distance
 
         runs.push(
           new Run(
@@ -79,21 +97,24 @@ function addLines(ev) {
             inputMin.value,
             coords,
             +distance,
-            generateColor()
+            generateColor(),
+            numOfRuns
           )
         );
         addPopUp(
           coords[coords.length - 1],
           `🏁Finish`,
           false,
-          `<p ${generateStyle()} class="text-tool-tip current-distance">🛣️ ${distance}km</p>`
+          `<p ${generateStyle(
+            numOfRuns
+          )} class="text-tool-tip current-distance">🛣️ ${distance}km</p>`,
+          numOfRuns
         );
         coords = [];
         distance = 0;
         currentCord = 0;
         numOfRuns++;
-        console.log(runs);
-
+        localStorage.setItem("runDave2024", JSON.stringify(runs));
         return;
       }
       document.querySelector(".submit").style.transform = "translateX(70px)";
@@ -105,7 +126,9 @@ function addLines(ev) {
 }
 
 function generateToolTipText() {
-  return `<p ${generateStyle()} class="text-tool-tip current-distance">🛣️ ${distance}km</p> <div class="submit"><input class="hidden" type="number" placeholder="Minutes"> <button class="ok"> Ok </button></div>`;
+  return `<p ${generateStyle(
+    numOfRuns
+  )} class="text-tool-tip current-distance">🛣️ ${distance}km</p> <div class="submit"><input class="hidden" type="number" placeholder="Minutes"> <button class="ok"> Ok </button></div>`;
 }
 
 function addMarker(crd) {
@@ -113,7 +136,7 @@ function addMarker(crd) {
     icon: myIcon,
   }).addTo(map);
 }
-function addPopUp(crd, content = "", close = false, content2 = "") {
+function addPopUp(crd, content = "", close = false, content2 = "", id) {
   return L.popup(crd, {
     minWidth: 60,
     maxWidth: 110,
@@ -124,8 +147,8 @@ function addPopUp(crd, content = "", close = false, content2 = "") {
     closeButton: false,
     closeOnEscapeKey: false,
     close: false,
-    content: `<p ${generateStyle()} ><span class="run">#${
-      numOfRuns + 1
+    content: `<p ${generateStyle(id)} ><span class="run">#${
+      id + 1
     }</span>${content}<br />${content2}</p>`,
     // content: `<p class="text-tool-tip">${content}</p> ${content2}`,
   }).openOn(map);
@@ -143,6 +166,7 @@ function mapInitialization(coords) {
   map = mapFunctions.generateMap(coords);
   mapFunctions.displayMap();
   mapFunctions.addMapClickEvent(addLines);
+  displayRuns();
 }
 /**
  * Calculates distance of line cordinates
@@ -154,3 +178,28 @@ function calculateDistance() {
   distance += Math.round(latlng1.distanceTo(latlng2)) / 1000;
   distance = +distance.toFixed(2);
 }
+
+/*OVERLAY**/
+const overlay = document.querySelector(".overlay-info");
+const overlayCont = document.querySelector(".info");
+
+function overlayRemove() {
+  overlay.classList.remove("hidden");
+  overlayCont.classList.remove("hidden");
+}
+
+function overlayAdd() {
+  overlay.classList.add("hidden");
+  overlayCont.classList.add("hidden");
+  console.log(overlayCont);
+}
+
+overlay.addEventListener("click", () => {
+  overlay.style.opacity = "0";
+  overlayCont.style.opacity = "0";
+  setTimeout(() => {
+    overlayAdd();
+  }, 1000);
+});
+
+overlayRemove();
